41eb2d3cde2c455f73adda9a577c57f8ef2b9a54
Dongximing <dongx34@myumanitoba.ca>
2022-06-08 09:32:48 +0800

    bert base

--- bert-chinese.py
+++ bert-chinese.py
@@ -36,6 +36,7 @@ def data_process(train_data_path, validation_data_path,test_data_path,tokenizer,
         if number == 100:
             break
         example, label = line.split("\t")
+        print(example) 
         training_example.append(example)
         training_label.append(int(label))
         number+=1
