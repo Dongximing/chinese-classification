75232c762b62f7881d47c244e83036138b2d6931
Dongximing <dongx34@myumanitoba.ca>
2022-06-11 16:32:57 +0800

    bert base

--- check.py
+++ check.py
@@ -1,14 +1,3 @@
-number = 0
-validation_example = []
-validation_label = []
-with open('/home/dongxx/projects/def-parimala/dongxx/chinese/Bert-Chinese-Text-Classification-Pytorch/THUCNews/data/dev.txt') as f1:
-    validation_lines = f1.readlines()
-
-for line in validation_lines:
-    if number == 100:
-        break
-    example, label = line.split("\t")
-    print(example)
-    validation_example.append(example)
-    validation_label.append(int(label))
-    number+=1
+list = ['a','b']
+res = {val: index+1 for index,val in enumerate(list)}
+print(res)
---
+++ prompt.py
@@ -0,0 +1,11 @@
+import openprompt
+import os
+from openprompt.data_utils.text_classification_dataset import PROCESSORS
+
+base_path = '/home/dongxx/projects/def-parimala/dongxx/chinese/Bert-Chinese-Text-Classification-Pytorch/THUCNews/data'
+dataset_name = 'prompt'
+dataset_path = os.path.join(base_path, dataset_name)
+processor = PROCESSORS[dataset_name.lower()]()
+trainvalid_dataset = processor.get_train_examples(dataset_path)
+assert processor.get_num_labels() == 2
+print(trainvalid_dataset[0])
