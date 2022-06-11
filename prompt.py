500048a55aa0bd59d697bd3dc6ab6f6421a07b81
Dongximing <dongx34@myumanitoba.ca>
2022-06-11 18:18:53 +0800

    1

--- prompt.py
+++ prompt.py
@@ -8,5 +8,5 @@ dataset_path = '/home/dongxx/projects/def-parimala/dongxx/chinese/Bert-Chinese-T
 processor = DataProcessor(labels_path=dataset_path)
 #trainvalid_dataset = processor.get_train_examples(data_dir="/home/dongxx/projects/def-parimala/dongxx/chinese/Bert-Chinese-Text-Classification-Pytorch/THUCNews/data/prompt/train.txt")
 print(processor.get_num_labels())
-trainvalid_dataset = processor.get_examples(data_dir=dataset_path)
+trainvalid_dataset = processor.get_train_examples()
 print(trainvalid_dataset[0])
