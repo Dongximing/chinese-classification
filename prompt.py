b8273a4071a9f5d8e39538c06ff6977c62d05827
Dongximing <dongx34@myumanitoba.ca>
2022-06-11 17:45:41 +0800

    bert base

--- prompt.py
+++ prompt.py
@@ -6,7 +6,7 @@ base_path = '/home/dongxx/projects/def-parimala/dongxx/chinese/Bert-Chinese-Text
 dataset_name = "prompt"
 dataset_path = os.path.join(base_path, dataset_name)
 print(dataset_path)
-processor = DataProcessor[dataset_name.lower()]()
-trainvalid_dataset = processor.get_train_examples(dataset_path)
+processor = DataProcessor(labels_path=dataset_path)
+trainvalid_dataset = processor.get_train_examples(data_dir="/home/dongxx/projects/def-parimala/dongxx/chinese/Bert-Chinese-Text-Classification-Pytorch/THUCNews/data/prompt/train.txt")
 assert processor.get_num_labels() == 2
 print(trainvalid_dataset[0])
