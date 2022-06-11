9d27a7819d339c251a7a68ce92731275a929d7e1
Dongximing <dongx34@myumanitoba.ca>
2022-06-11 17:32:01 +0800

    bert base

--- prompt.py
+++ prompt.py
@@ -3,8 +3,9 @@ import os
 from openprompt.data_utils.text_classification_dataset import PROCESSORS
 
 base_path = '/home/dongxx/projects/def-parimala/dongxx/chinese/Bert-Chinese-Text-Classification-Pytorch/THUCNews/data/'
-dataset_name = 'prompt'
+dataset_name = "prompt"
 dataset_path = os.path.join(base_path, dataset_name)
+print(dataset_path)
 processor = PROCESSORS[dataset_name.lower()]()
 trainvalid_dataset = processor.get_train_examples(dataset_path)
 assert processor.get_num_labels() == 2
